23fdca821864caeda4a7f5d06c332fcf3f9fb0b8
Robert Plawski <dev@robertplawski.pl>
2025-08-10 15:06:56 +0200

    Templated a contact and testimonials section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,17 @@
 import ProfessionalExperience from "./components/sections/ProfessionalExperience";
 import Hero from "./components/sections/Hero";
 import { Footer } from "./components/sections/Footer";
+// import Testimonials from "./components/sections/Testimonials";
+// import Contact from "./components/sections/Contact";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <ProfessionalExperience />
+      {/*<Testimonials />*/}
       {/*<Portfolio />*/}
+      {/*<Contact />*/}
       <Footer />
     </>
   );
