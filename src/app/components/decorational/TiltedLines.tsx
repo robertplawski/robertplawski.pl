50e55d3de20ff68e1e73d558d6f64c89a125d8dc
Robert Pławski <dev@robertplawski.pl>
2026-02-07 16:26:52 +0100

    quickfix: fixed titled lines eyecandy

--- src/app/components/decorational/TiltedLines.tsx
+++ src/app/components/decorational/TiltedLines.tsx
@@ -1,3 +1,3 @@
 export default function TiltedLines() {
-  return <div className='opacity-40 border-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:var(--color-black)]/5 max-lg:h-66 max-lg:border-t lg:border-l dark:[--pattern-fg:var(--color-white)]/10 w-full h-full top-0 left-0 absolute'></div>
+  return <div className='opacity-40 border-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:var(--color-black)]/5 max-lg:border-t lg:border-l dark:[--pattern-fg:var(--color-white)]/10 w-full h-full top-0 left-0 absolute'></div>
 }
